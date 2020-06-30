74ac481a551fbd0fc3106b36a3e7bbe27549b573
ASO1801006 <1801006@s.asojuku.ac.jp>
2020-06-30 05:40:41 +0000

    コミットメッセージ

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: "HelloWorld",
+    props:{
+      title: String,
+      message: String,
+    }
+  }
+</script>
+
+<style>
+  div{
+    margin: 0px;
+    padding: 0px;
+    text-align:left;
+  }
+  hi{
+    font-size: 72pt;
+    font-weight: bold;
+    text-align: right;
+    letter-spacing: -8px;
+    color: #f0f0f0;
+    margin: 0px
+  }
+  p{
+    margin: 0px;
+    color: #666;
+    
+  }
+</style>
